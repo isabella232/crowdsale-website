db5e23437f62976ff46977fd731d253adc72d628
Nicolas Gotchac <ngotchac@gmail.com>
2017-10-17 13:05:06 +0200

    Clean-up config

--- backend/config/default.js
+++ backend/config/default.js
@@ -1,4 +1,6 @@
-module.exports = {
+const dev = require('./dev');
+
+module.exports = Object.assign({
   http: {
     hostname: '127.0.0.1',
     port: 4000
@@ -8,4 +10,4 @@ module.exports = {
     port: 6379
   },
   nodeWs: 'ws://127.0.0.1:8546/'
-};
+}, dev);
